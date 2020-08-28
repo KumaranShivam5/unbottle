21fb4a09f0b9f7fd49fd945cfe341f0c0e48e6d2
ushasi-bhowmick <64476667+ushasi-bhowmick@users.noreply.github.com>
2020-08-28 21:47:15 +0530

    add mid end

---
+++ insta_assets/insta_mid.js
@@ -0,0 +1,58 @@
+var apiurl = 'https://ushasi.pythonanywhere.com/';
+
+Vue.component ( 'instacard', {
+	template: `<div class="grid-item" v-on:click="$emit('open', scard.post_url)">
+                    <div class='grid-post-overlay'>
+                        open Post
+                    </div>
+                    <img :src="scard.post_url+'media/'" />
+                </div>`,
+	data: function () {
+		return {
+		data_apiurl:apiurl
+		}
+	},
+	props:["scard"]
+});
+
+var insta_disp =new Vue ({
+	el:'#insta-mid-end',
+	
+	data: {
+		insta_array:[],
+	},
+	
+	methods: {
+		open_post(url) {
+			var righturl= url + '?utm_source=ig_embed&amp;utm_campaign=loading';
+            $('.post-popup').css('z-index', 2);
+            $('.post-popup').css('opacity', 1);
+            $('.window-wrapper').css('width', '80%');
+			$('.instagram-media').attr('data-instgrm-permalink', righturl);
+			$('.to-change').attr('href',righturl);
+        },
+		
+	},
+	
+	mounted (){ 
+		axios.get(apiurl+'kumaran/view-all-insta-post/')
+			 .then(res => {
+				 this.insta_array=JSON.parse(JSON.stringify(res.data));
+			 });
+			 
+	},	
+	
+	updated() {
+		var $grid = $('.grid').masonry({
+            itemSelector: '.grid-item',
+            percentPosition: true,
+            columnWidth: '.grid-sizer'
+        });
+		$grid.imagesLoaded().progress(function() {
+            $grid.masonry();
+        });
+		
+	},
+
+
+});
