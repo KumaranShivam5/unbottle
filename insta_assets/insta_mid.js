0ff0b168ff2633c00f804d0d03329a8e04528e1a
KumaranShivam5 <kumaranshivam57@gmail.com>
2020-08-29 01:12:45 +0530

    insta post reload issue resolve

--- insta_assets/insta_mid.js
+++ insta_assets/insta_mid.js
@@ -1,73 +1,81 @@
 var apiurl = 'https://ushasi.pythonanywhere.com/';
 
-Vue.component ( 'instacard', {
-	template: `<div class="grid-item" v-on:click="$emit('open', scard.post_url)">
+Vue.component('instacard', {
+    template: `<div class="grid-item" v-on:click="$emit('open', scard.post_url)">
                     <div class='grid-post-overlay'>
                         open Post
                     </div>
                     <img :src="scard.post_url+'media/'" />
                 </div>`,
-	data: function () {
-		return {
-		data_apiurl:apiurl
-		}
-	},
-	props:["scard"]
+    data: function() {
+        return {
+            data_apiurl: apiurl
+        }
+    },
+    props: ["scard"]
 });
 
-var insta_disp =new Vue ({
-	el:'#insta-mid-end',
-	
-	data: {
-		insta_array:[],
-	},
-	
-	methods: {
-		open_post(url) { 
-			var righturl= url + '?utm_source=ig_embed&amp;utm_campaign=loading';
-			$('.instagram-media').attr('data-instgrm-permalink', righturl);
-			$('.to-change').attr('href',righturl);
-			
-			this.reloadJs("//www.instagram.com/embed.js");
-			console.log($('.instagram-media').attr('data-instgrm-permalink'));
-			$('.post-popup').css('z-index', 2);
+var insta_disp = new Vue({
+    el: '#insta-mid-end',
+
+    data: {
+        insta_array: [],
+    },
+
+    methods: {
+        open_post(url) {
+            var righturl = url + '?utm_source=ig_embed&amp;utm_campaign=loading';
+            $('.instagram-media').attr('data-instgrm-permalink', righturl);
+            $('.to-change').attr('href', righturl);
+
+            //this.reloadJs("//www.instagram.com/embed.js");
+            this.reloadInstaEmbed()
+
+            console.log($('.instagram-media').attr('data-instgrm-permalink'));
+            $('.post-popup').css('z-index', 2);
             $('.post-popup').css('opacity', 1);
             $('.window-wrapper').css('width', '80%');
 
-			
-        	},
-		
-		reloadJs(src) {
-			console.log("called reload",src);
-			$('#random').remove();
-			let myScript = document.createElement("script");
-			myScript.setAttribute("src", src);
-			myScript.setAttribute("id", 'random');
-			document.getElementById('test').appendChild(myScript);
-			console.log("called reload",src);
-		},
-		
-	},
-	
-	mounted (){ 
-		axios.get(apiurl+'kumaran/view-all-insta-post/')
-			 .then(res => {
-				 this.insta_array=JSON.parse(JSON.stringify(res.data));
-			 });
-			 
-	},	
-	
-	updated() {
-		var $grid = $('.grid').masonry({
+
+        },
+
+        reloadJs(src) {
+            console.log("called reload", src);
+            $('#random').remove();
+            let myScript = document.createElement("script");
+            myScript.setAttribute("src", src);
+            myScript.setAttribute("id", 'random');
+            document.getElementById('test').appendChild(myScript);
+            console.log("called reload", src);
+        },
+
+        reloadInstaEmbed() {
+            // reload_post();
+            instgrm.Embeds.process();
+        },
+
+
+    },
+
+    mounted() {
+        axios.get(apiurl + 'kumaran/view-all-insta-post/')
+            .then(res => {
+                this.insta_array = JSON.parse(JSON.stringify(res.data));
+            });
+
+    },
+
+    updated() {
+        var $grid = $('.grid').masonry({
             itemSelector: '.grid-item',
             percentPosition: true,
             columnWidth: '.grid-sizer'
         });
-		$grid.imagesLoaded().progress(function() {
+        $grid.imagesLoaded().progress(function() {
             $grid.masonry();
         });
-		
-	},
+
+    },
 
 
 });
