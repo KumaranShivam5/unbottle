201333501ea3f34bb2e5b91e91e6fad6c120741b
KumaranShivam5 <kumaranshivam57@gmail.com>
2020-09-09 01:53:06 +0530

    new worls

--- admin_assets/login_sys.js
+++ admin_assets/login_sys.js
@@ -12,25 +12,25 @@ function getCookie(cname) {
         }
     }
     return "";
-} 
+}
 //dont convert these into axios calls, or move inside vue
 var token = getCookie('token')
 if (!token) {
     window.location.href = 'admin-login.html';
-} 
-else {
+} else {
     var xhr = new XMLHttpRequest();
     //xhr.withCredentials = true;
-    xhr.addEventListener("readystatechange", function () {
+    xhr.addEventListener("readystatechange", function() {
         if (this.readyState === 4) {
             stat = (JSON.parse(this.responseText)).verified;
-            if(!stat){window.location.href='admin-login.html'}
+            if (!stat) { window.location.href = 'admin-login.html' }
         }
     });
-    xhr.open("GET", "https://ushasi.pythonanywhere.com/kumaran/api-token-auth/");
+    xhr.open("GET", "https://ushasi.pythonanywhere.com/kumaran/is-user/");
     xhr.setRequestHeader("Authorization", `Token ${token}`);
     xhr.send();
 }
+
 function logout() {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.href = 'admin-login.html';
