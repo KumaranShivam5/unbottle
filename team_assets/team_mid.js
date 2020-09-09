b71cd6586742c6a7788aca2c95da83e49d8f4b80
ushasi-bhowmick <64476667+ushasi-bhowmick@users.noreply.github.com>
2020-09-09 21:43:50 +0530

    add mid end

--- team_assets/team_mid.js
+++ team_assets/team_mid.js
@@ -1,16 +1,21 @@
 var apiurl = 'https://ushasi.pythonanywhere.com/';
 
 Vue.component ( 'teamcard', {
-	template: `<div class="team-card">
-			<img class="team-card-img" :src="this.data_apiurl+scard.member_image">
-				<div class="team-card-desc">
-					<h1>{{scard.name}}</h1>
-					<p>{{scard.quote}}</p>
-					<div class="team-card-tag">{{scard.desg}}</div>
-					<div class="team-card-tag">{{scard.emai_id}}</div>
-					<div class="team-card-tag">{{scard.contact_no}}</div>
-				</div>
-			</div>`,
+	template: `<div class="trial-card">
+                    <div class="trial-card-wrap">
+                        <div class="trial-card-skew"></div>
+                        <div class="trial-card-img" :style="{'background-image':'url('+data_apiurl+scard.member_image+')'}"></div>
+                        <div class="trial-overlay-dark"></div>
+                        <div class="trial-overlay">
+                            <p class="overlay-quote">{{scard.quote}}</p>
+                            <div class="trial-card-details">
+                                <span class="trial-card-detail">{{scard.name}}</span>
+                                <span class="trial-card-detail">{{scard.desg}}</span>
+                                <span class="trial-card-detail">{{scard.emai_id}}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>`,
 	data: function () {
 		return {
 		data_apiurl:apiurl
@@ -19,6 +24,7 @@ Vue.component ( 'teamcard', {
 	props:["scard"]
 });
 
+//:style="{'background-image':'url('+data_apiurl+scard.member_image+')'}"
 var team_disp =new Vue ({
 	el:'#team-mid-end',
 	
